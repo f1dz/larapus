3b594e39be6b917e77301dd0ac982c5db2944f9f
Khofidin <offiedz@gmail.com>
2019-02-27 08:54:17 +0700

    4.5 Persiapan Relasi One-to-Many di Eloquent

--- app/Author.php
+++ app/Author.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $fillable = ['name'];
+
+    public function books() {
+        return $this->hasMany('App\Book');
+    }
 }
